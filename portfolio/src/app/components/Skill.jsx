19adb34e954742d572a0365af50de6026bb175b6
Julian Riera <105263588+Srezequielr@users.noreply.github.com>
2023-07-27 18:16:15 -0300

    feature-skills-01

---
+++ portfolio/src/app/components/Skill.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+export default function Skill({ data }) {
+  return (
+    <div className="rounded col-span-1 bg-slate-500 p-5">
+      <div>
+        <h4 className="text-center text-2xl mb-3">{data.name}</h4>
+      </div>
+      <div className="flex flex-row m-auto justify-center items-center">
+        <Image alt={data.name} className="h-24 w-24 object-contain" src={data.img} />
+      </div>
+    </div>
+  );
+}
--- portfolio/src/app/components/Skills.jsx
+++ portfolio/src/app/components/Skills.jsx
@@ -1,7 +1,69 @@
+import Skill from "./Skill";
+import cssIcon from "./../../Images/cssBK.png";
+import figmaIcon from "./../../Images/figmaBK.png";
+import HTMLBKIcon from "./../../Images/HTMLBK.png";
+import insomniaIcon from "./../../Images/insomniaBK.png";
+import javascriptIcon from "./../../Images/javascriptBK.png";
+import materialUIIcon from "./../../Images/material-uiBK.png";
+import reactIcon from "./../../Images/reactBK.png";
+import reduxIcon from "./../../Images/reduxBK.png";
+import expressIcon from "./../../Images/expressjsBK.png";
+import gitHunIcon from "./../../Images/githubBK.png";
+import slackIcon from "./../../Images/slackBK.png";
+import trelloIcon from "./../../Images/trelloBK.png";
+import vsCodeIcon from "./../../Images/visual-studio-codeBK.png";
+import adobeIcon from "./../../Images/adobe-illustratorBK.png";
+
+const languajes = [
+  { name: "HTML", img: HTMLBKIcon },
+  { name: "Java Script", img: javascriptIcon },
+  { name: "CSS", img: cssIcon },
+];
+
+const technologies = [
+  { name: "React", img: reactIcon },
+  { name: "Redux", img: reduxIcon },
+  { name: "Material UI", img: materialUIIcon },
+  { name: "Express", img: expressIcon },
+];
+
+const tools = [
+  { name: "GitHub", img: gitHunIcon },
+  { name: "Adobe Ilustrator", img: adobeIcon },
+  { name: "Trello", img: trelloIcon },
+  { name: "Figma", img: figmaIcon },
+  { name: "Insomnia", img: insomniaIcon },
+  { name: "Slack", img: slackIcon },
+  { name: "VS Code", img: vsCodeIcon },
+];
+
 export default function Skills() {
   return (
-    <div>
-      <p>Soy tus Skils</p>
-    </div>
+    <article className="container m-auto px-8">
+      <div>
+        <h2 className="text-4xl font-bold">Tech Skills</h2>
+        <br />
+        <h3 className="text-2xl font-bold pb-6">Lenguages</h3>
+        <div className="grid grid-cols-3 gap-12">
+          {languajes.map((languaje) => (
+            <Skill data={languaje} />
+          ))}
+        </div>
+        <br />
+        <h3 className="text-2xl font-bold pb-6">Tecnologias</h3>
+        <div className="grid grid-cols-4 gap-12">
+          {technologies.map((technologie) => (
+            <Skill data={technologie} />
+          ))}
+        </div>
+        <br />
+        <h3 className="text-2xl font-bold pb-6">Herramientas de trabajo</h3>
+        <div className="grid grid-cols-4 gap-12">
+          {tools.map((tool) => (
+            <Skill data={tool} />
+          ))}
+        </div>
+      </div>
+    </article>
   );
 }
