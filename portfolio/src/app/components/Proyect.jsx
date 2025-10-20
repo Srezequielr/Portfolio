b9617110be1c8fb144d6d5984e346589a185ed73
Julian <julianrierawrk@gmail.com>
2025-10-20 01:11:51 -0300

    Cambio de promanitas al proyecto de campirteria y agregue tecnologias usadas en proyectis

--- portfolio/src/app/components/Proyect.jsx
+++ portfolio/src/app/components/Proyect.jsx
@@ -1,31 +1,49 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Icon } from "@iconify/react";
+import { skillsData } from "../skills";
 
 export default function Proyect({ data }) {
+  console.log(data);
+
   return (
     <div className="bg-mainLight dark:bg-secondDark rounded-3xl shadow-lg">
       <Image src={data.img} alt={data.name} className="rounded-t-3xl " />
-      <h4 className="m-4 font-bold text-3xl md:text-4xl text-white dark:text-black ">
+      <h4 className="m-4 font-bold text-2xl md:text-3xl text-white dark:text-black ">
         {data.name}
       </h4>{" "}
       <p className="m-4">{data.description}</p>
+      <div className="m-4 flex flex-wrap gap-3">
+        {data.stack.map((tech, index) => (
+          <div
+            title={tech}
+            key={index}
+            className=" bg-detailColor text-white dark:text-black rounded-lg p-2 text-sm font-semibold"
+          >
+            <Icon className="text-4xl" icon={skillsData[tech]} />
+          </div>
+        ))}
+      </div>
       <div className="flex gap-3 mb-4 ml-4 justify-start items-center">
-        <Link
-          href={data.gitHub}
-          target="_blank"
-          className="shadow-2xl rounded-lg bg-detailColor p-1 hover:shadow-none transition"
-        >
-          <Icon
-            icon={"line-md:github"}
-            className="inline text-4xl md:text-5xl "
-          ></Icon>
-        </Link>
-        <Link href={data.deploy} target="_blank">
-          <p className="font-bold text-xl md:text-2xl shadow-2xl rounded-lg bg-detailColor text-center p-2 md:p-3 hover:shadow-none transition">
-            Deploy
-          </p>
-        </Link>
+        {data.gitHub ? (
+          <Link
+            href={data.gitHub}
+            target="_blank"
+            className="shadow-2xl rounded-lg bg-detailColor p-1 hover:shadow-none transition"
+          >
+            <Icon
+              icon={"line-md:github"}
+              className="inline text-4xl md:text-5xl "
+            ></Icon>
+          </Link>
+        ) : null}
+        {data.deploy ? (
+          <Link href={data.deploy} target="_blank">
+            <p className="font-bold text-xl md:text-2xl shadow-2xl rounded-lg bg-detailColor text-center p-2 md:p-3 hover:shadow-none transition">
+              Deploy
+            </p>
+          </Link>
+        ) : null}
       </div>
     </div>
   );
--- portfolio/src/app/components/Proyects.jsx
+++ portfolio/src/app/components/Proyects.jsx
@@ -1,29 +1,60 @@
 import promanitasPh from "./../../Images/promanitas.png";
 import comidaPh from "./../../Images/comida+.png";
+import SCHPh from "./../../Images/SCH.png";
 import pokeAppPh from "./../../Images/pokeApp.png";
 import Proyect from "./Proyect";
 
 const proyects = [
   {
-    name: "Promanitas",
-    img: promanitasPh,
-    deploy: "https://pro-manitas-client.vercel.app/",
-    gitHub: "https://github.com/ProManitas",
-    description: "",
+    name: "Sistemas de Control Horario",
+    img: SCHPh,
+    deploy: null,
+    gitHub: null,
+    stack: [
+      "JavaScript",
+      "Supabase",
+      "Github",
+      "Nextjs",
+      "NodeJS",
+      "HTML",
+      "Tailwind",
+      "Figma",
+      "Trello",
+    ],
+    description: "Aplicacion desarrollada para la getion de horarios de una organizacion, resolviendo el problema de registros de horarios y sueldo de empleados. La aplicación permite a los empleados registrar sus horas de entrada y salida, capturando la ubicación exacta mediante geolocalización para validar el fichaje. Basado en las horas trabajadas y el sueldo configurado, el sistema calcula automáticamente el saldo total a cobrar. El objetivo principal de esta aplicación es optimizar la gestión de horarios y pagos, proporcionando una solución eficiente y precisa para las organizaciones y sus empleados, siendo esta facil de utilizar para los empleados y administradores.",
   },
   {
     name: "Comida +",
     img: comidaPh,
     deploy: "https://pi-food-main-gt2pubtvb-srezequielr.vercel.app/",
     gitHub: "https://github.com/Srezequielr/PI-Food-main",
-    description: "",
+    stack: [
+      "JavaScript",
+      "React",
+      "NodeJS",
+      "Express",
+      "PostgreSQL",
+      "HTML",
+      "Redux",
+    ],
+    description: "Desarrollé una SPA (Single Page Application) como proyecto final para Henry. La aplicación consume intensivamente una API externa para buscar, filtrar y mostrar recetas, y permite a los usuarios crear y almacenar sus propias recetas en una base de datos PostgreSQL propia. Implementé un backend robusto con Node.js y Express para gestionar toda la lógica de negocio y la API REST. El frontend se construyó con React y Redux para un manejo de estado eficiente, permitiendo a los usuarios filtrar, ordenar y crear recetas de forma dinámica.",
   },
   {
     name: "PokeApp",
     img: pokeAppPh,
     deploy: "https://pi-pokemon-main-pi.vercel.app/",
     gitHub: "https://github.com/Srezequielr/PI-Pokemon-main",
-    description: "",
+    stack: [
+      "JavaScript",
+      "React",
+      "NodeJS",
+      "Express",
+      "PostgreSQL",
+      "HTML",
+      "Redux",
+      "Material UI",
+    ],
+    description: "esarrollé una SPA de recetas 100% responsive, enfocada en una experiencia de usuario limpia y adaptativa. La aplicación consume una API externa para buscar y filtrar recetas, y permite a los usuarios crear y guardar las suyas propias en una base de datos PostgreSQL. El backend se construyó con Node.js y Express. Para el frontend, utilicé React y Redux para el manejo de estado, implementando todo el diseño con Material UI para asegurar que la interfaz sea funcional y elegante en cualquier dispositivo, desde móviles hasta computadoras de escritorio.",
   },
 ];
 
@@ -34,7 +65,7 @@ export default function Proyects() {
         <h2 className="text-4xl sm:text-6xl dark:text-white font-bold my-5">
           Projects &gt;
         </h2>
-        <div className="grid grid-cols-1 gap-6  md:grid-cols-2 lg:grid-cols-3 md:gap-12">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 md:gap-7">
           {proyects.map((proyect, index) => (
             <Proyect key={index} data={proyect} />
           ))}
